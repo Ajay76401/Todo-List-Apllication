faf9faf6ab9a5764dfb88f6d70bf8fd1591353e9
Ajay Nalwade <ajaynalwade321@gmail.com>
2025-08-02 22:50:52 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app =express();
+app.set("view engine","ejs");
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"))
+
+
+
+var items = [];
+app.get("/",function(req,res){
+    res.render("list",{ejes :items})
+
+})
+
+app.post("/",function(req,res){
+    var item =req.body.input1;
+    items.push(item);
+    res.redirect("/");
+})
+app.listen(8000,function(){
+    console.log("server Running...");
+})
